8fc3b6031b3e379056fdbf4a9d98d87b6a9a322f
raoul <manu34@free.fr>
2015-03-04 21:31:08 +0100

    [skip ci] add comments

--- src/base/IWorkflowDefinitionProvider.php
+++ src/base/IWorkflowDefinitionProvider.php
@@ -1,10 +1,15 @@
 <?php
 namespace raoul2000\workflow\base;
 
+/**
+ * This interface must be implemented by any PHP class that
+ * is able to provide a workflow definition. 
+ */
 interface IWorkflowDefinitionProvider
 {
 	/**
-	 *
+	 * Returns the workflow definition in the form of an array.
+	 * @return array
 	 */
 	public function getDefinition();
 }
